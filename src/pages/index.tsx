29b31ac56433e016e8c0ea19fb913965d177fc0a
Leonardo Giroto <leonardo_giroto@hotmail.com>
2023-04-17 18:30:33 -0300

    feat: adds project and innaccessible examples

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,4 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from 'next/head';
 
 export default function Home() {
   return (
@@ -14,109 +9,79 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
+      <main>
+        <h1>Bad Accessibility is worse than no accessibility</h1>
+        <p>The goal of this website is to prove that you can have a perfect score regarding accessibility and SEO in Web Vitals, as well as automated tools audit passing for accessibility tests and still have a <strong>poor accessibility experience for users.</strong> Check some examples below:</p>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
+        <h2>1. Images and Alternative Texts</h2>
+
+        <div className="imagesContainer">
+          <img
+            src="https://images.pexels.com/photos/9123067/pexels-photo-9123067.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="Picture of a tall blond woman, wearing a beautiful brown dress and white tennis shoes, standing in front of a commercial stablishment named Orpheum and which has a lettering saying 'Every human is an artist. The dream of your life is to make beautiful art."
+            width="360"
           />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
+
+          <img
+            src="https://images.pexels.com/photos/6996360/pexels-photo-6996360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="two women and a baking machine"
+            width="360"
+          />
+
+          <img
+            src="https://images.pexels.com/photos/16378938/pexels-photo-16378938.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="pexels-photo-16378938.jpeg"
+            width="360"
+          />
+        </div>
+
+        <p>The first picture has an extremely wordy alternative text, which is going to take a lot of time for assistive technologies to read and that has a lot of information that is not necessary.</p>
+
+        <p>The second picture has an alternative text that provides almost no information regarding the image. Does it provide the required context for people using asssitive technologies? </p>
+
+        <p>The third picture has an alternative text that is the name of the picture file - no description, but which will be read by assistive technologies.</p>
+
+        <h2>2. Aria Hidden</h2>
+
+        <p>Aria Hidden is used to hide content from assistive technologies in order to improve the experience. Also, tabIndex can be set to prevent focus on interactive elements. However, if it is used to hide essential content, people using those technologies won't be able to access them - for instance, promotions and coupons.</p>
+        
+        <div className="banner" aria-hidden>
+          <p>Promotion: click button below for discount</p>
+          <button type="button" tabIndex={-1}>Get Discount</button>
+        </div>
+
+        <h2>3. Forms</h2>
+
+        <p>
+          Visual Bias makes us believe that some form are functional and compreensible.
+          <br />
+          However, focus order is different from what we visually see and this form is going to intercalete address fields with unrelated ones. Try tabbing.
+        </p>
+
+        <form>
+          <div>
+            <input type="text" placeholder="Name" aria-label="Name" />
+            <input type="text" placeholder="Street" aria-label="Address 1" />
           </div>
-        </div>
+          <div>
+            <input type="text" placeholder="E-mail" aria-label="E-mail"  />
+            <input type="text" placeholder="Neighborhood" aria-label="Address 2"  />
+          </div>
+          <div>
+            <input type="text" placeholder="Phone Number" aria-label="Phone Number"  />
+            <input type="text" placeholder="City" aria-label="Address 3"  />
+          </div>
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
+          <button type="button">Enviar</button>
+        </form>
 
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
+        <h2>And more...</h2>
 
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        <p>
+          What is shown here are just some practical examples of bad accessibility with valid markup.
+          <br />
+          Good accessibility requires <strong>empathy</strong> and <strong>testing with real users</strong>.
+        </p>
       </main>
     </>
   )
